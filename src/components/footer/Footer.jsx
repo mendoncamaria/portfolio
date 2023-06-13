9c7b8687fd7445731c38420a291337f91409980b
Maria Mendonca <maria.mendonca156@gmail.com>
2023-06-13 10:42:25 +0530

    Changed footer content, added styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
+import styles from './footer.module.css';
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+  return <div className={styles.container}>&copy; MARIA MENDONCA</div>;
 }
 
-export default Footer
+export default Footer;
