cc3444f3f6fdcdc4c7fadc875e606fc11a7cbcaa
Maria Mendonca <maria.mendonca156@gmail.com>
2023-06-13 14:24:51 +0530

    Added skill icons, fixed height

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import cssIcon from '../../assets/css3.png'
+import htmlIcon from '../../assets/html5.png'
+import javascriptIcon from '../../assets/javascript.png'
+import reactIcon from '../../assets/react.png'
+import reduxIcon from '../../assets/redux.png'
 
 function Skills() {
   return (
-    <div>Skills</div>
-  )
+    <>
+    <img alt="skill" src={cssIcon} height="250px" width="250px" />
+    <img alt="skill" src={htmlIcon} height="250px" width="250px" />
+    <img alt="skill" src={javascriptIcon} height="250px" width="250px" />
+    <img alt="skill" src={reactIcon} height="250px" width="250px" />
+    <img alt="skill" src={reduxIcon} height="250px" width="250px" />
+    </>
+  );
 }
 
-export default Skills
+export default Skills;
