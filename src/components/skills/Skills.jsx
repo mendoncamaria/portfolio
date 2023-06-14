e6f3f45490e87fa4245195bc0a9e9eed4311f665
Maria Mendonca <maria.mendonca156@gmail.com>
2023-06-14 09:36:12 +0530

    added extra skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -4,15 +4,29 @@ import htmlIcon from '../../assets/html5.png'
 import javascriptIcon from '../../assets/javascript.png'
 import reactIcon from '../../assets/react.png'
 import reduxIcon from '../../assets/redux.png'
+import githubicon from '../../assets/github.png'
+import mongoDBIcon from '../../assets/mongoDB.png'
+import mySQLIcon from '../../assets/mySQL.png'
+import pythonIcon from '../../assets/python.png'
+import sassIcon from '../../assets/sass.png'
+import vscodeIcon from '../../assets/vscode.png'
 
 function Skills() {
   return (
     <>
-    <img alt="skill" src={cssIcon} height="250px" width="250px" />
-    <img alt="skill" src={htmlIcon} height="250px" width="250px" />
-    <img alt="skill" src={javascriptIcon} height="250px" width="250px" />
-    <img alt="skill" src={reactIcon} height="250px" width="250px" />
-    <img alt="skill" src={reduxIcon} height="250px" width="250px" />
+    <div className="heading">SKILLS</div>
+    <div>
+    <img alt="skill" src={cssIcon} height="150px" width="150px" />
+    <img alt="skill" src={htmlIcon} height="150px" width="150px" />
+    <img alt="skill" src={javascriptIcon} height="150px" width="150px" />
+    <img alt="skill" src={reactIcon} height="150px" width="150px" />
+    <img alt="skill" src={reduxIcon} height="150px" width="150px" />
+    <img alt="skill" src={githubicon} height="150px" width="150px" />
+    <img alt="skill" src={mongoDBIcon} height="150px" width="150px" />
+    <img alt="skill" src={mySQLIcon} height="150px" width="150px" />
+    <img alt="skill" src={pythonIcon} height="150px" width="150px" />
+    <img alt="skill" src={sassIcon} height="150px" width="150px" />
+    <img alt="skill" src={vscodeIcon} height="150px" width="150px" /></div>
     </>
   );
 }
