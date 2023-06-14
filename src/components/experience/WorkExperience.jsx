12b4660ac670523129f46c40e4c10258e3c68230
Maria Mendonca <maria.mendonca156@gmail.com>
2023-06-14 09:36:36 +0530

    added data for work experience

--- src/components/experience/WorkExperience.jsx
+++ src/components/experience/WorkExperience.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
 
 function WorkExperience() {
   return (
-    <div>WorkExperience</div>
-  )
+    <>
+      <div className="heading">WORK EXPERIENCE</div>
+      <div>
+        <div>
+          <div>
+            <div className="title">Cloud Associate</div>
+            <div className="employer">Niveus Solutions</div>
+          </div>
+          <div>Jan/2022-July/2023</div>
+        </div>
+        <div>
+          <ul>
+            <li>
+              Successfully handled a new module with 10 services, providing
+              support and training to 4 juniors and ensuring the project was
+              completed with very little rework
+            </li>
+            <li>
+              Took responsibility for Sonarqube cleanup and development,
+              providing knowledge transfer and support development for UI faults
+              in other modules
+            </li>
+            <li>
+              Applied the Atomic Structure of React Development to the current
+              module and prepared and maintained documentation for the assigned
+              and existing modules
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default WorkExperience
+export default WorkExperience;
