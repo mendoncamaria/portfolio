404c5405a750a00b1da43e4333b7bba09f214c33
Maria Mendonca <maria.mendonca156@gmail.com>
2023-06-14 09:36:23 +0530

    added data for aboutme

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -3,14 +3,48 @@ import maria from '../../assets/maria.jpg';
 
 function AboutMe() {
   return (
-    <>
-      <div>
-        About Me
+    <div
+      style={{
+        display: 'flex',
+      }}
+    >
+      <div
+        style={{
+          width: '70%',
+        }}
+      >
+        <p>
+          Welcome to my portfolio! I am an aspiring web developer with 1 year of
+          experience in building web applications. During my journey, I had the
+          opportunity to work on a banking website, where I gained valuable
+          insights into the industry and honed my skills in creating
+          user-friendly interfaces. Although I have primarily focused on a
+          single project, I have also invested time in personal projects to
+          expand my knowledge and explore different aspects of web development.
+          <br />
+          One of my key areas of expertise is ReactJS. I thoroughly enjoy
+          leveraging its capabilities to create dynamic and responsive web
+          applications. I am always eager to learn and grow, and I am open to
+          collaborating with like-minded individuals and teams to take on new
+          and exciting challenges in the field of web development.
+          <br />
+          Feel free to explore my portfolio and get in touch with me if you're
+          interested in working together or if you have any questions. Let's
+          bring our ideas to life through the power of web development!
+          <br />
+        </p>
       </div>
       <div>
-        <img src={maria} alt="Maria" width="250px" height="350px" />
+        <img
+          src={maria}
+          alt="Maria"
+          style={{
+            width: '200px',
+            height: 'auto',
+          }}
+        />
       </div>
-    </>
+    </div>
   );
 }
 
