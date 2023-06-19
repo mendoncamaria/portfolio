17bc854eb64f3b3eb6f76aab49217b18f73b5ced
Maria Mendonca <maria.mendonca156@gmail.com>
2023-06-19 11:57:10 +0530

    Moved skills out of main page- to separate file

---
+++ src/components/skills/SkillList.js
@@ -0,0 +1,196 @@
+import PythonIcon from '../../assets/python.png';
+import JavascriptIcon from '../../assets/javascript.png';
+
+import ReactIcon from '../../assets/reactJS.png';
+import ReduxIcon from '../../assets/redux.png';
+import ReactNativeIcon from '../../assets/react.png';
+import HtmlIcon from '../../assets/html5.png';
+import CssIcon from '../../assets/css3.png';
+import SassIcon from '../../assets/sass.png';
+
+import NodeJSIcon from '../../assets/nodeJS.png';
+
+import MongoDBIcon from '../../assets/mongoDB.png';
+import MySQLIcon from '../../assets/mySQL.png';
+
+import GithubIcon from '../../assets/github.png';
+import GitlabIcon from '../../assets/gitlab.png';
+
+import RedmineIcon from '../../assets/redmine.png';
+import AgileIcon from '../../assets/agile.png';
+
+import VSCodeIcon from '../../assets/vscode.png';
+import VisualStudioIcon from '../../assets/visual-studio.png';
+import NotepadPlusPlus from '../../assets/notepasPlusPlus.png';
+
+import WindowsIcon from '../../assets/windows.png';
+import UbuntuIcon from '../../assets/ubuntu.png';
+
+import FirebaseIcon from '../../assets/firebase.png';
+import AwsIcon from '../../assets/aws.png';
+import GcpIcon from '../../assets/gcp.png';
+
+export const GeneralPurposeLanguageList = [
+  {
+    src: PythonIcon,
+    alt: 'Python',
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'JavaScript',
+    src: JavascriptIcon,
+    height: '150px',
+    width: '150px',
+  },
+];
+
+export const FrontendSkillList = [
+  {
+    alt: 'React',
+    src: ReactIcon,
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'Redux',
+    src: ReduxIcon,
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'react-native',
+    src: ReactNativeIcon,
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'HTML',
+    src: HtmlIcon,
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'CSS',
+    src: CssIcon,
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'SASS',
+    src: SassIcon,
+    height: '150px',
+    width: '150px',
+  },
+];
+
+export const BackendSkillList = [
+  {
+    alt: 'NodeJS',
+    src: NodeJSIcon,
+    height: '150px',
+    width: '250px',
+  },
+];
+
+export const DatabaseList = [
+  {
+    alt: 'MongoDB',
+    src: MongoDBIcon,
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'MySQL',
+    src: MySQLIcon,
+    height: '150px',
+    width: '150px',
+  },
+];
+
+export const VersionControlSystemsList = [
+  {
+    alt: 'Github',
+    src: GithubIcon,
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'Gitlab',
+    src: GitlabIcon,
+    height: '150px',
+    width: '150px',
+  },
+];
+
+export const ProjectManagementToolsList = [
+  {
+    alt: 'Redmine',
+    src: RedmineIcon,
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'Agile',
+    src: AgileIcon,
+    height: '150px',
+    width: '150px',
+  },
+];
+
+export const CodeEditorsList = [
+  {
+    alt: 'VS-Code',
+    src: VSCodeIcon,
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'Visual-Studio-Code',
+    src: VisualStudioIcon,
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'Notepad-Plus-Plus',
+    src: NotepadPlusPlus,
+    height: '150px',
+    width: '150px',
+  },
+];
+
+export const OperatingSystemsList = [
+  {
+    alt: 'Windows',
+    src: WindowsIcon,
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'Ubuntu',
+    src: UbuntuIcon,
+    height: '150px',
+    width: '150px',
+  },
+];
+
+export const CloudServicesList = [
+  {
+    alt: 'Firebase',
+    src: FirebaseIcon,
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'AWS',
+    src: AwsIcon,
+    height: '150px',
+    width: '150px',
+  },
+  {
+    alt: 'GCP',
+    src: GcpIcon,
+    height: '150px',
+    width: '150px',
+  },
+];
