759e0706d433ccf1660b9d7e9ab870b6f7d2a633
Maria Mendonca <maria.mendonca156@gmail.com>
2023-06-20 09:41:32 +0530

    Contact me form- basic structure laid

--- src/components/contactMe/Contact.jsx
+++ src/components/contactMe/Contact.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import styles from './contact.module.css';
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <h1 className="container">Contact Me</h1>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
+        <div className={styles.left}></div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '500px',
+          }}
+        >
+          <input
+            type="text"
+            className={styles.inputStyles}
+            placeholder="Enter text"
+          />
+          <input
+            type="text"
+            className={styles.inputStyles}
+            placeholder="Enter text"
+          />
+          <textarea
+            type="text"
+            className={styles.inputStyles}
+            placeholder="Enter text"
+          />
+          <button>submit</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
