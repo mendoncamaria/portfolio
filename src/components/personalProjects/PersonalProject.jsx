5977a2dd0a67ccf8b609e6e3a18cb5c0c3dd39d5
Maria Mendonca <maria.mendonca156@gmail.com>
2023-06-22 23:20:54 +0530

    added project, style

--- src/components/personalProjects/PersonalProject.jsx
+++ src/components/personalProjects/PersonalProject.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import styles from './personalProject.module.css';
+import { FaGithub, FaGlobe } from 'react-icons/fa';
 
 function PersonalProject() {
   return (
     <div>
-      <p className={styles.heading}>Personal Projects</p>
+      <p className={styles.heading}>Projects I have worked on</p>
+      <div style={{ backgroundColor: 'blue', width: '230px', height: '230px' }}>
+        <p style={{ color: 'white' }}>React, Firebase</p>
+        <h1 style={{ fontSize: '3em' }}>Mini Projects</h1>
+        <div style={{ display: 'flex', gap: '20px' }}>
+          <p>
+            <FaGithub />
+            Github
+          </p>
+          <p>
+            <FaGlobe />
+            Website
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
