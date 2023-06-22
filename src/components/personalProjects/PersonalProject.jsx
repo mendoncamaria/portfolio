83692a8ef6e980f04db74ce7ceb480066a96efec
Maria Mendonca <maria.mendonca156@gmail.com>
2023-06-22 23:48:56 +0530

    moved common code to card component

--- src/components/personalProjects/PersonalProject.jsx
+++ src/components/personalProjects/PersonalProject.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import styles from './personalProject.module.css';
-import { FaGithub, FaGlobe } from 'react-icons/fa';
+import Card from '../../common/Card';
 
 function PersonalProject() {
   return (
     <div>
       <p className={styles.heading}>Projects I have worked on</p>
-      <div style={{ backgroundColor: 'blue', width: '230px', height: '230px' }}>
-        <p style={{ color: 'white' }}>React, Firebase</p>
-        <h1 style={{ fontSize: '3em' }}>Mini Projects</h1>
-        <div style={{ display: 'flex', gap: '20px' }}>
-          <p>
-            <FaGithub />
-            Github
-          </p>
-          <p>
-            <FaGlobe />
-            Website
-          </p>
-        </div>
+      <div style={{display: 'flex', flexDirection: 'row', gap: '1rem'}}>
+        <Card projectName="Mini Projects" language="React, Firebase" />
+        <Card projectName="Todo List" language="React, Firebase" />
       </div>
     </div>
   );
