512c4d7bc588e1e0e3ce7298df82bbae8fc8e2f6
Maria Mendonca <maria.mendonca156@gmail.com>
2023-07-06 17:30:48 +0530

    changed main bg color, designed work section, moved constants out

--- src/common/textConstants.js
+++ src/common/textConstants.js
@@ -1,3 +1,11 @@
 export const TEXT_CONSTANTS = {
-// TODO: MOVE CONSTANTS HERE
-}
+  WORK_EXPERIENCE: 'Work Experience',
+};
+
+export const WORK_EXPERIENCE1 = {
+  DATE: 'Jan/2022 - July/2023',
+  NAME: 'NIVEUS SOLUTIONS PVT LTD',
+  TITLE: 'Cloud Associate- Development',
+  POINT1:
+    'Successfully managed a new module with 10 services, overseeing the support and training of 4 juniors while ensuring minimal rework, and effectively completed the project. Additionally, took charge of Sonarqube cleanup and development, providing knowledge transfer and resolving UI faults in other modules, while applying the Atomic Structure of React Development to the current module and maintaining comprehensive documentation.',
+};
--- src/components/experience/WorkExperience.jsx
+++ src/components/experience/WorkExperience.jsx
@@ -1,40 +1,25 @@
 import React from 'react';
 import styles from './experience.module.css';
+import { TEXT_CONSTANTS, WORK_EXPERIENCE1 } from '../../common/textConstants';
+import img from '../../assets/niveus.jpg'
 
 function WorkExperience() {
   return (
     <>
       <div className="heading">
-        <p className={styles.heading}>
-          Work Experience
-        </p>
+        <p className={styles.heading}>{TEXT_CONSTANTS.WORK_EXPERIENCE}</p>
       </div>
-      <div>
-        <div>
-          <div>
-            <div className="title">Cloud Associate</div>
-            <div className="employer">Niveus Solutions</div>
+      <div className={styles.main}>
+        <div className={styles.sub_container}>
+          <div className="image">
+          <img src={img} width='200px' height= '190px' style={{borderRadius: '10px'}} alt='img' />
           </div>
-          <div>Jan/2022-July/2023</div>
-        </div>
-        <div>
-          <ul>
-            <li>
-              Successfully handled a new module with 10 services, providing
-              support and training to 4 juniors and ensuring the project was
-              completed with very little rework
-            </li>
-            <li>
-              Took responsibility for Sonarqube cleanup and development,
-              providing knowledge transfer and support development for UI faults
-              in other modules
-            </li>
-            <li>
-              Applied the Atomic Structure of React Development to the current
-              module and prepared and maintained documentation for the assigned
-              and existing modules
-            </li>
-          </ul>
+          <div className="content">
+            <h1 className="companyName">{WORK_EXPERIENCE1.NAME}</h1>
+            <p className="jobTitle">{WORK_EXPERIENCE1.TITLE}</p>
+            <p className="dates">{WORK_EXPERIENCE1.DATE}</p>
+            <p className="contentData">{WORK_EXPERIENCE1.POINT1}</p>
+          </div>
         </div>
       </div>
     </>
